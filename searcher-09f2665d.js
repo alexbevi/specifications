fad8814c3cacdff181be0d67c3277565cdf1c7d3
alexbevi <alexbevi@users.noreply.github.com>
2026-06-19 11:58:39 +0000

    deploy: a103118ebb7bed98bc1517ce6cd03f90adf7daa6

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-946962eb.js',
+                path_to_root + 'searchindex-90921c4d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
